c8be6c1fbeb615dc653efe3720643a95ee7435c1
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2023-08-15 15:42:27 +0530

    Profile changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 640px) { ... }
+    },
     extend: {
       colors: {
         fuchsia: colors.fuchsia,
@@ -13,4 +17,5 @@ module.exports = {
   },
   plugins: [],
 }
+
 
